2931dca10a57bbc6ba1bab21020dcf7317759f0a
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-13 18:06:32 +0530

    Create Home Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Header from "./component/layout/Header/Header";
 import Footer from "./component/layout/Footer/Footer";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ src/component/Home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { CgMouse } from "react-icons/cg";
+import './Home.css'
+const Home = () => {
+  return (
+    <>
+      <div className="banner">
+        <p>Welcome to Ecommerce</p>
+        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+        <a href="#container">
+          <button>Scroll <CgMouse /></button>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Home;
