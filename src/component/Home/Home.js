294067f532831994134b0ba60d09d15db04657cc
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-14 20:20:59 +0530

    introduce redux and get all products using redux

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import Product from "./Product";
 import MetaData from "../layout/MetaData";
-
-const product = {
-  name: "Blue Tshirt",
-  image: [{ url: "https://ibb.co/QK5gqtB" }],
-  price: "3000",
-  _id: "naveen",
-};
+import { getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
 const Home = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if(error){
+      return alert.error(error)
+    }
+    dispatch(getProduct());
+  }, [dispatch, error]);
+
   return (
     <>
-      <MetaData title="ECOMMERCE" />
-      <div className="banner">
-        <p>Welcome to Ecommerce</p>
-        <h1>FIND AMAZING PRODUCTS BELOW</h1>
-        <a href="#container">
-          <button>
-            Scroll <CgMouse />
-          </button>
-        </a>
-      </div>
-      <h2 className="homeHeading">Featured Products</h2>
-      <div className="container" id="container">
-        <Product product={product} />
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MetaData title="ECOMMERCE" />
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+          <h2 className="homeHeading">Featured Products</h2>
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => <Product product={product} />)}
+          </div>
+        </>
+      )}
     </>
   );
 };
