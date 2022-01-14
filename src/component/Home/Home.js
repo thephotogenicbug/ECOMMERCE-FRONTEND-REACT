81bb11991093efd67f3a2f46b35aec213bf68da9
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-14 14:41:07 +0530

    format code and install npm pacakages

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,30 +1,32 @@
 import React from "react";
 import { CgMouse } from "react-icons/cg";
-import './Home.css'
+import "./Home.css";
 import Product from "./Product";
 import MetaData from "../layout/MetaData";
 
 const product = {
   name: "Blue Tshirt",
   image: [{ url: "https://ibb.co/QK5gqtB" }],
-  price:"3000",
-  _id:"naveen"
+  price: "3000",
+  _id: "naveen",
 };
 
 const Home = () => {
   return (
     <>
-       <MetaData title="ECOMMERCE" />
+      <MetaData title="ECOMMERCE" />
       <div className="banner">
         <p>Welcome to Ecommerce</p>
         <h1>FIND AMAZING PRODUCTS BELOW</h1>
         <a href="#container">
-          <button>Scroll <CgMouse /></button>
+          <button>
+            Scroll <CgMouse />
+          </button>
         </a>
       </div>
       <h2 className="homeHeading">Featured Products</h2>
       <div className="container" id="container">
-          <Product product={product} />
+        <Product product={product} />
       </div>
     </>
   );
